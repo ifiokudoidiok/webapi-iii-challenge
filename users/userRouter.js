16bc9d0e360860ba56b0fdcdeb9d3e226d0953c2
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-30 15:48:59 +0100

    POST User API endpoint set up

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,11 +6,24 @@ const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
 userRouter.get('/:id',validateUserId, getUserById);
-// userRouter.post('/', postUser);
+userRouter.post('/', validateUser , postUser);
 // userRouter.post('/:id/posts', createUsersPost);
 // userRouter.get('/:id/posts', getUsersPost);
 // userRouter.delete('/:id', deleteUser);
 // userRouter.put('/:id', editUser);
+
+function postUser(req, res) {
+    dbUser.insert(req.body)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(error => {
+    
+        res.status(500).json({
+          message: 'Error adding the new user: ' + error.message,
+        });
+      })
+}
 
 function getUserById(req, res){
     res.json(req.user);
@@ -37,17 +50,32 @@ function validateUserId(req, res, next) {
             req.user = user;
             next()
         }else{
-            res.status(404).json({ message: 'Post id does not correspond with an actual post' });
+            res.status(404).json({ message: 'User id does not correspond with an actual User' });
         }
     })
     .catch(error => {
-        res.status(404).json({ message: 'Something terrible happend while checking hub id: ' + error.message,})
+        res.status(404).json({ message: "invalid user id: "  + error.message})
     })
+
 }
 
-function validateUser(req, res, next) {
 
-};
+function validateUser(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if(req.body.name){
+            next();
+
+        }
+        else
+        {
+            res.status(400).json({ message: "missing required name field" });
+        }
+      } 
+      else 
+      {
+        res.status(400).json({ message: "missing user data" });
+      }
+}
 
 function validatePost(req, res, next) {
 
