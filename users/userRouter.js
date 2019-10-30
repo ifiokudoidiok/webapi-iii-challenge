628a25bcde96b332424dacc9766470545d30873a
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-30 17:01:35 +0100

    MVP completed; stretch attempted

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,16 +1,59 @@
 const express = require('express');
 
 const dbUser = require('./userDb');
+const dbPost = require('../posts/postDb')
 
 const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
 userRouter.get('/:id',validateUserId, getUserById);
 userRouter.post('/', validateUser , postUser);
-// userRouter.put('/:id',validateUserId,validateUser, editUser);
 userRouter.delete('/:id',validateUserId, deleteUser);
-// userRouter.post('/:id/posts', createUsersPost);
-// userRouter.get('/:id/posts', getUsersPost);
+userRouter.put('/:id', validateUserId, validateUser, editUser);
+userRouter.get('/:id/posts',validateUserId,  getUsersPost);
+userRouter.post('/:id/posts',validateUserId, validatePost, createUsersPost);
+
+function createUsersPost(req, res) {
+    dbPost.insert({text:req.body.text, user_id:req.user.id})
+    .then(created => {
+        res.status(201).json(created)
+    })
+    .catch(error => {
+    
+        res.status(500).json({
+          message: 'Error : ' + error.message,
+        });
+      })
+}
+
+function getUsersPost(req, res) {
+    dbUser.getUserPosts(req.user.id)
+    .then(posts => {
+        res.status(200).json({
+            success: true,
+            posts
+        })
+    })
+    .catch(error => {
+    
+        res.status(500).json({
+          message: 'Error : ' + error.message,
+        });
+      })
+}
+
+function editUser(req,res){
+    dbUser.update(req.user.id, req.body)
+    .then(() => {
+        res.status(200).json({...req.user, ...req.body})
+        
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Could not Update Post: " + error
+        })
+    })
+}
 
 function deleteUser(req, res){
     dbUser.remove(req.user.id)
@@ -89,7 +132,20 @@ function validateUser(req, res, next) {
 }
 
 function validatePost(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if(req.body.text){
+            next();
 
-};
+        }
+        else
+        {
+            res.status(400).json({ message: "missing required text field" });
+        }
+      } 
+      else 
+      {
+        res.status(400).json({ message: "missing post data" });
+      }
+}
 
 module.exports = userRouter;
