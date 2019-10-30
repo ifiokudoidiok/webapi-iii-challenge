cd2b6ad597f4095e8f56ebfca6b4661aaf06c569
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-30 14:34:24 +0100

    server set up

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(helmet());
+
+server.use(express.json());
+server.use('/posts', postRouter);
+server.use('/users', userRouter);
+
+server.get('/users', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+server.get('/posts', (req, res) => {
+  res.send('Posts api is running')
+})
+server.get('/users', (req, res) => {
+  res.send('Users api is running')
+})
 
 //custom middleware
 
